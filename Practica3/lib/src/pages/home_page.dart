1f3f6229b65f262eb54fbe275f811b8ba146e1ac
JavierCopete <javier.copete@sarria.salesians.catS>
2020-11-07 16:16:50 +0100

    Practica3

---
+++ Practica3/lib/src/pages/home_page.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/search/search_delegate.dart';
+
+import 'package:peliculas/src/widgets/card_swiper_widget.dart';
+import 'package:peliculas/src/widgets/movie_horizontal.dart';
+
+class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
+  @override
+  Widget build(BuildContext context) {
+    peliculasProvider.getPopulares();
+
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          title: Text('Mejores Series'),
+          backgroundColor: Colors.amber, //Cambiar titulo y color
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search_sharp), //cambiar icono busqueda
+              onPressed: () {
+                showSearch(
+                  context: context,
+                  delegate: DataSearch(),
+                  // query: 'Hola'
+                );
+              },
+            )
+          ],
+        ),
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[_swiperTarjetas(), _footer(context)],
+          ),
+        ));
+  }
+
+  Widget _swiperTarjetas() {
+    return FutureBuilder(
+      future: peliculasProvider.getEnCines(),
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
+        } else {
+          return Container(
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
+        }
+      },
+    );
+  }
+
+  Widget _footer(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text('Series Populares',
+                  style:
+                      Theme.of(context).textTheme.subhead) //Cambiar subtitulo
+              ),
+          SizedBox(height: 5.0),
+          StreamBuilder(
+            stream: peliculasProvider.popularesStream,
+            builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+              if (snapshot.hasData) {
+                return MovieHorizontal(
+                  peliculas: snapshot.data,
+                  siguientePagina: peliculasProvider.getPopulares,
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
